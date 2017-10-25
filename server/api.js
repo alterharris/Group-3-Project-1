224c8cf10baea395a708b92b3427def65bcf4598
emd0008 <emd0008@uah.edu>
2017-10-25 11:44:03 -0500

    Update 4

--- server/api.js
+++ server/api.js
@@ -3,8 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const categories_ctrl_1 = require("./controllers/categories.ctrl");
 const payment_ctrl_1 = require("./controllers/payment.ctrl");
+const contactform_ctrl_1 = require("./controllers/contactform.ctrl");
 const router = express.Router();
 router
-    .use('/categories', categories_ctrl_1.default)
-    .use('/payment', payment_ctrl_1.default);
+    .use("/categories", categories_ctrl_1.default)
+    .use("/payment", payment_ctrl_1.default)
+    .use("/contactforms", contactform_ctrl_1.default);
 exports.default = router;
---
+++ server/controllers/contactform.ctrl.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const email_svc_1 = require("../services/email.svc");
+const router = express_1.Router();
+//actually api/contactform
+router.post("/", (req, res) => {
+    email_svc_1.sendEmail("BKB4979@gmail.com", req.body.from, "Covalence Store Inquiry", req.body.message)
+        .then(response => {
+        res.sendStatus(201);
+    })
+        .catch(e => {
+        console.log(e);
+        res.sendStatus(500);
+    });
+});
+exports.default = router;
