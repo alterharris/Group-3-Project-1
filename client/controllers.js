3f631f5d655ca44622f6b72a5a45c98630100442
emd0008 <emd0008@uah.edu>
2017-10-25 09:51:31 -0500

    Credit Card scripts added

--- client/controllers.js
+++ client/controllers.js
@@ -1,1 +1,16 @@
 angular.module('Project1.controllers', [])
+.controller('PaymentController', ['$scope', function($scope){
+    let elements = stripe.elements();
+    let card = elements.create('card');
+    card.mount('#card-field');
+
+    $scope.process = function(){
+        stripe.createToken(card).then((result) => {
+            if(result.error){
+                $scope.error = result.error.message;
+            }else{
+                res.send(result.card);
+            }
+        });
+    }
+}]);
