ac1dd6233b83ae2953b286e395e3f24ed6f0799a
Brad <31662261+TideJeep@users.noreply.github.com>
2017-10-25 10:53:54 -0500

    Setting up email

--- server/services/email.svc.ts
+++ server/services/email.svc.ts
@@ -1,3 +1,16 @@
 import * as sendgrid from "sendgrid";
 const helper = sendgrid.mail;
-const sg = sendgrid(process.env.SENDGRID_AVI_KEY || "");
+const sg = sendgrid(process.env.SENDGRID_API_KEY || "");
+
+export function sendMail(
+  to: string,
+  from: string,
+  subject: string,
+  message: string
+) {
+  let toEmail = new helper.Email(to);
+  let fromEmail = new helper.Email(from, "Covalence Group 3");
+  let emailContent = new helper.Content("text/html", message);
+
+  let mail = new helper.Mail(fromEmail, subject, toEmail, emailContent);
+}
