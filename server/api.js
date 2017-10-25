64a4ac480b3e6518c462b5205c134cba514f1a5f
emd0008 <emd0008@uah.edu>
2017-10-25 09:55:22 -0500

    update 2

--- server/api.js
+++ server/api.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const categories_ctrl_1 = require("./controllers/categories.ctrl");
+const payment_ctrl_1 = require("./controllers/payment.ctrl");
 const router = express.Router();
 router
-    .use('/categories', categories_ctrl_1.default);
+    .use('/categories', categories_ctrl_1.default)
+    .use('/payment', payment_ctrl_1.default);
 exports.default = router;
