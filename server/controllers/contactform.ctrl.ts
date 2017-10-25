77f5457803f4aaba73ea47fe720d226989c6c9d0
Brad <31662261+TideJeep@users.noreply.github.com>
2017-10-25 11:11:17 -0500

    Email backup is done, ready for Contact Form view

--- server/controllers/contactform.ctrl.ts
+++ server/controllers/contactform.ctrl.ts
@@ -3,6 +3,21 @@ import { sendEmail } from "../services/email.svc";
 
 const router = Router();
 
-router.post("/", (req, res) => {});
+//actually api/contactform
+router.post("/", (req, res) => {
+  sendEmail(
+    "BKB4979@gmail.com",
+    req.body.from,
+    "Covalence Store Inquiry",
+    req.body.message
+  )
+    .then(response => {
+      res.sendStatus(201);
+    })
+    .catch(e => {
+      console.log(e);
+      res.sendStatus(500);
+    });
+});
 
 export default router;
