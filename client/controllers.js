aa57c5c90781c32d90bdfeff884dd1cfee992b6c
emd0008 <emd0008@uah.edu>
2017-10-26 11:27:28 -0500

    added payment button and ref to cart

--- client/controllers.js
+++ client/controllers.js
@@ -6,8 +6,10 @@ angular.module("Project1.controllers", []).controller("PaymentController", ["$sc
         stripe.createToken(card).then(result => {
           if (result.error) {
             $scope.error = result.error.message;
+            alert("There is a problem with your payment!")
           } else {
             res.send(result.card);
+            alert("Thank you for your payment!")
           }
         });
       };
