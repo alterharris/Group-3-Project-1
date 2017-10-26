f38e01dcbf091063ee92749cc72b9c44255a2d6d
alterharris <31677867+alterharris@users.noreply.github.com>
2017-10-26 15:46:19 -0500

    add process.env.PORT

--- server/index.ts
+++ server/index.ts
@@ -35,8 +35,9 @@ app.get("*", (req, res, next) => {
   }
 });
 
-app.listen(3000);
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000.');
+});
 function isAsset(path: string) {
   let pieces = path.split("/");
   if (pieces.length === 0) {
